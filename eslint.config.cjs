86b08dda49b35def634bec098e83e55a3dc1d1bb
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2025-05-24 03:48:09 +0900

    add: GitHub ActionsによるCI

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,6 +1,9 @@
 // eslint.config.cjs
 module.exports = [
   {
+    ignores: ['node_modules/', 'dist/'],
+  },
+  {
     files: ['**/*.js', '**/*.cjs'],
     languageOptions: {
       ecmaVersion: 2021,
